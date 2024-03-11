92606d35b550cc4cc8be9610095f1e7d5374c3dc
JoshOuma <136029030+JoshOuma@users.noreply.github.com>
2024-03-11 04:04:44 -0700

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
-#!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log('0');
-} else {
-  const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (a, b) { return b - a; })[1];
-  console.log(second);
+function findSecondLargest(...args) {
+    const integers = args.map(Number);
+    const sortedIntegers = integers.sort((a, b) => b - a);
+    if (sortedIntegers.length < 2) {
+        console.log(0);
+    } else {
+        console.log(sortedIntegers[1]);
+    }
 }
